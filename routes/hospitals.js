f97a9c0a6c22f0e80fe43649002206313e9092ed
priesdelly <priesdelly@gmail.com>
2023-02-04 16:37:33 +0700

    move business logic to controller

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,39 +1,19 @@
 const express = require('express');
+const {
+  getHospitals,
+  getHospital,
+  createHospital,
+  updateHospital,
+  deleteHospital,
+} = require('../controllers/hospitals');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show all hospitals',
-  });
-});
+router.route('/').get(getHospitals).post(createHospital);
 
-router.get('/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: `Show hospitals ${req.params.id}`,
-  });
-});
-
-router.post('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Create new hospitals',
-  });
-});
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: `Update hospitals ${req.params.id}`,
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    msg: `Delete hospitals ${req.params.id}`,
-  });
-});
+router
+  .route('/:id')
+  .get(getHospital)
+  .put(updateHospital)
+  .delete(deleteHospital);
 
 module.exports = router;
